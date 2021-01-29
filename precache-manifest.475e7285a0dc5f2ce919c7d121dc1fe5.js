6854c05a57cd38b0e3e97f094f49c053e1f459fa
= <jdavid@yext.com>
2021-01-28 23:49:23 -0500

    Updates

---
+++ precache-manifest.475e7285a0dc5f2ce919c7d121dc1fe5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edbefcd2d249cc332a4",
+    "url": "/cardgame/static/js/app.70a504f8.chunk.js"
+  },
+  {
+    "revision": "6ee8d68c4d1b9dc627c1",
+    "url": "/cardgame/static/js/runtime~app.108cf9dd.js"
+  },
+  {
+    "revision": "ee6e7f33ae79d475d6d5",
+    "url": "/cardgame/static/js/2.cf8803ec.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/cardgame/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/cardgame/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/cardgame/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/cardgame/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/cardgame/favicon.ico"
+  },
+  {
+    "revision": "e5316f9956ee0e982d5cbb6db5e92efc",
+    "url": "/cardgame/index.html"
+  },
+  {
+    "revision": "22c6c23e4846e123776160f660a286a8",
+    "url": "/cardgame/manifest.json"
+  },
+  {
+    "revision": "4eb6ee15afeaa96fbbbfb3b22b2ed86e",
+    "url": "/cardgame/register-service-worker.js"
+  }
+];
